ae84bfc1da85f0c72c9ed6db43ae2f794cb7667b
Jack Parry <jack.parry@theinformationlab.com>
2025-08-12 19:24:16 +0000

    building payment staging model

---
+++ models/staging/stg_stripe__payment.sql
@@ -0,0 +1,29 @@
+--imports
+with 
+
+source as (
+
+    select * 
+    
+    from raw.stripe.payment
+
+), 
+
+--final CTE
+renamed as (
+    select 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount,
+    created as created_at,
+    _batched_at
+
+from source
+
+)
+--final select
+select * 
+
+from renamed
